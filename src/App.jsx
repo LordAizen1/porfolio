da15a89917903d4ce70acbf149d853bc487eea0d
Mohammad Kaif <mohdkaif2003@gmail.com>
2025-03-08 19:32:27 +0530

    redirecting issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header'; // <-- Import Header
-import Navbar from './components/Navbar'; // <-- Import Navbar
-import Footer from './components/Footer'; // <-- Import Footer
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import KnowMore from './pages/KnowMore';
 import ProjectsPage from './pages/ProjectsPage';
@@ -10,8 +10,8 @@ import './style.css';
 
 function App() {
   return (
-    <Router>
-      <Header /> {/* Use Header here */}
+    <Router basename="/porfolio">
+      <Header />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
