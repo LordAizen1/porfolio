df3feb56ea3a98becbd27916a63ccc8125ca27f8
Mohammad Kaif <mohdkaif2003@gmail.com>
2025-03-09 12:42:14 +0530

    added a scroll to top button

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import KnowMore from './pages/KnowMore';
 import ProjectsPage from './pages/ProjectsPage';
+import ScrollToTopButton from './components/ScrollToTopButton'; // Import the ScrollToTopButton
 import './style.css';
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Route path="/projects" element={<ProjectsPage />} />
       </Routes>
       <Footer />
+      <ScrollToTopButton /> {/* Add the ScrollToTopButton here */}
     </Router>
   );
 }
