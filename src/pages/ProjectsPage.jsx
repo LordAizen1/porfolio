298743484da4b261eb25dbee5e60227208fd1e8d
Mohammad Kaif <mohdkaif2003@gmail.com>
2025-04-30 12:27:55 +0530

    minor update

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -5,17 +5,17 @@ import ParticlesComponent from '../components/Particles';
 const ProjectsPage = () => {
   const projects = [
     {
-      title: 'Dr-WebDev',
-      image: '/porfolio/images/dr-webdev-high-resolution-logo 1.png',
-      description:
-        'Dr-WebDev is one of the fun projects I\'ve made. <br /> Creating this project was like hitting two birds with one stone. <br /> I got to learn front-end development and let people know about me.',
-    },
-    {
       title: 'VeilCode Labs',
       image: '/porfolio/images/veilcodelabs.png',
       description:
         "An educational cryptography platform built with React.js and Firebase. Features course materials, labs, and research resources with admin CRUD functionality. Developed under Dr. Ravi Anand at IIIT Delhi. The project aims to enhance cryptography education through a structured and accessible digital learning environment.",
       link: 'https://cryptolab.iiitd.edu.in/'
+    },
+    {
+      title: 'Dr-WebDev',
+      image: '/porfolio/images/dr-webdev-high-resolution-logo 1.png',
+      description:
+        'Dr-WebDev is one of the fun projects I\'ve made. <br /> Creating this project was like hitting two birds with one stone. <br /> I got to learn front-end development and let people know about me.',
     },   
     {
       title: 'StickHero',
