fc05a016fb4c87828a74cd47ac59a0a91ed5b23e
LordAizen1 <mohdkaif2003@gmail.com>
2025-06-04 09:59:54 +0530

    new minor updated

--- src/pages/KnowMore.jsx
+++ src/pages/KnowMore.jsx
@@ -91,6 +91,8 @@ const KnowMore = () => {
             { name: 'NodeJS', icon: '/porfolio/images/nodejs.png' },
             { name: 'Kaggle', icon: '/porfolio/images/kaggle.png' },
             { name: 'Tableau', icon: '/porfolio/images/tableau.png'},
+            { name: 'n8n', icon: '/porfolio/images/n8n.png'},
+            { name: 'Make', icon: '/porfolio/images/make.png'},
           ].map((technology, index) => (
             <motion.div
               key={technology.name}
@@ -116,6 +118,30 @@ const KnowMore = () => {
       >
         <h2>Achievements</h2>
         <div className="achievements-list">
+        <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.1, delay: 0.1 }}
+            whileHover={{ scale: 1.05 }}
+            className="achievement"
+          >
+            <a 
+              href="https://www.credly.com/badges/6badd752-6eb5-4921-8d4e-2fc5bb36c14c/linked_in_profile"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="achievement-link"
+            >
+              <img
+                src="/porfolio/images/google-advanced-data-analytics-certificate.png"
+                alt="Achievement 1"
+                className="achievement-icon"
+              />
+              <p>
+                <strong>Google's Advanced Data Analytics</strong>
+                Completed comprehensive training in data analysis and machine learning, mastering techniques like logistic regression, random forest, and decision trees. Gained hands-on experience in data preprocessing, feature engineering, and model evaluation. Applied these skills to real-world datasets, developing predictive models and extracting actionable insights.            
+              </p>
+            </a>
+          </motion.div>
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
@@ -123,15 +149,22 @@ const KnowMore = () => {
             whileHover={{ scale: 1.05 }}
             className="achievement"
           >
-            <img
-              src="/porfolio/images/qiskit-global-summer-school-2024-quantum-excellence.png"
-              alt="Achievement 1"
-              className="achievement-icon"
-            />
-            <p>
-              <strong>IBM QGSS</strong>
-              Selected amongst thousands of participants to be a part of the Summer School.
-            </p>
+            <a 
+              href="https://www.credly.com/badges/fc613fda-7639-42ad-b4c6-6316cf0afeaf/public_url"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="achievement-link"
+            >
+              <img
+                src="/porfolio/images/qiskit-global-summer-school-2024-quantum-excellence.png"
+                alt="Achievement 1"
+                className="achievement-icon"
+              />
+              <p>
+                <strong>IBM QGSS</strong>
+                Selected amongst thousands of participants to be a part of the Summer School. Completed 4 tasks/jobs in IBM's Cloud Quantum Computers. 
+              </p>
+            </a>
           </motion.div>
           <motion.div
             initial={{ opacity: 0, y: 20 }}
@@ -140,15 +173,22 @@ const KnowMore = () => {
             whileHover={{ scale: 1.05 }}
             className="achievement"
           >
-            <img
-              src="/porfolio/images/artificial-intelligence-fundamentals.png"
-              alt="IBM AI Fundamentals Badge"
-              className="achievement-icon"
-            />
-            <p>
-              <strong>IBM Artificial Intelligence Fundamentals</strong>
-              Successfully completed the IBM Artificial Intelligence Fundamentals course, demonstrating proficiency in core AI concepts and applications as well as some experience with IBM Watson AI Studio.
-            </p>
+            <a 
+              href="https://www.credly.com/badges/6badd752-6eb5-4921-8d4e-2fc5bb36c14c/linked_in_profile"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="achievement-link"
+            >
+              <img
+                src="/porfolio/images/artificial-intelligence-fundamentals.png"
+                alt="IBM AI Fundamentals Badge"
+                className="achievement-icon"
+              />
+              <p>
+                <strong>IBM Artificial Intelligence Fundamentals</strong>
+                Successfully completed the IBM Artificial Intelligence Fundamentals course, demonstrating proficiency in core AI concepts and applications as well as some experience with IBM Watson AI Studio.
+              </p>
+            </a>
           </motion.div>
         </div>
       </motion.section>
